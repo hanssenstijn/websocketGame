862bec52dfa64e9fef698832d8d0076523933c2d
hanssenstijn <hanssenstijn@gmail.com>
2020-06-20 12:14:11 +0200

    setting connection

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 const http = require("http");
+const { request } = require("express");
+const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
 httpServer.listen(9090, () => console.log("Listening.. on 9090"));
 
+const wsServer = new websocketServer({
+  "httpServer": httpServer
+});
+
+wsServer.on("request", request => {
+  // connect
+  const connection = request.accept(null, request.origin);
+  connection.on("open", () => console.log("Opened!"));
+  connection.on("close", () => console.log("Closed!"));
+  connection.on("message", message => {
+
+    // I have recieved a message from the client
+
+  });
+
+
+})
