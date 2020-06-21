373033cbac1cc6f99b257b2c33f8dc7c9f4d02c9
hanssenstijn <hanssenstijn@gmail.com>
2020-06-21 11:58:11 +0200

    set up server, make connection

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const websocketServer = require("websocket").server;
 const httpServer = http.createServer();
 httpServer.listen(9090, () => console.log("Listening.. on 9090"));
 
+// hashmap clients
+const clients = {};
+
 const wsServer = new websocketServer({
   "httpServer": httpServer
 });
@@ -14,10 +17,29 @@ wsServer.on("request", request => {
   connection.on("open", () => console.log("Opened!"));
   connection.on("close", () => console.log("Closed!"));
   connection.on("message", message => {
-
+    const result = JSON.parse(message.utf8Data)
     // I have recieved a message from the client
-
+    console.log(result)
   });
 
+  // generate a new clientId
+  const clientId = guid();
+  clients[clientId] = {
+    "connection": connection
+  }
+
+  const payLoad = {
+    "method": "connect",
+    "clientId": clientId
+  }
+  // send back the client connect
+  connection.send(JSON.stringify(payLoad));
 
 })
+
+function S4() {
+  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
